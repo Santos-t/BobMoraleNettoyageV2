0ba6e434320f998e576d5e9d8f2bec6a3550a0a3
Antoine LEGRAND <legrand.antoine96@gmail.com>
2019-01-16 22:00:16 +0100

    Added templates

--- src/main.go
+++ src/main.go
@@ -7,21 +7,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Data struct {
-	Title string
+type HomeData struct {
+	IsAClient bool
 }
 
 var path = "src/templates"
-var home = template.Must(template.ParseFiles(path + "/index.html"))
+var home = template.Must(template.ParseFiles(path + "/home.html"))
+
+//var login = template.Must(template.ParseFiles(path + "/login.html"))
+//var signup = template.Must(template.ParseFiles(path + "/signup.html"))
+//var buildings = template.Must(template.ParseFiles(path + "/buildings.html"))
+//var myTickets = template.Must(template.ParseFiles(path + "/myTickets.html"))
+//var planning = template.Must(template.ParseFiles(path + "/planning.html"))
+//var profile = template.Must(template.ParseFiles(path + "/profile.html"))
+//var submittedTickets = template.Must(template.ParseFiles(path + "/submittedTickets.html"))
+//var ticketForm = template.Must(template.ParseFiles(path + "/ticketForm.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	data := Data{
-		Title: "coucou",
+	data := HomeData{
+		IsAClient: true,
 	}
 	home.Execute(w, data)
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
 
