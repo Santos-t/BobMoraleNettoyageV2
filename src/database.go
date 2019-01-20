6d235cedc390056b0142a260f1043f9858089175
Theo Santos <theosnake01@gmail.com>
2019-01-20 20:11:31 +0100

    Better indentation for the database.go file

--- src/database.go
+++ src/database.go
@@ -25,100 +25,107 @@ func init_db() {
     "phone_number TEXT NOT NULL, " +
     "address TEXT, " +
     "role TEXT " +
-    ")")
-    check_err(err)
-    statement.Exec()
+    ")",
+  )
+  check_err(err)
+  statement.Exec()
 
-    //Table building
-    statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS building (" +
-      "id INTEGER PRIMARY KEY, " +
-      "address TEXT NOT NULL, " +
-      "complement TEXT, " +
-      "floor_nb INTEGER, " +
-      "owner_id INTEGER " +
-      ")")
-      check_err(err)
-      statement.Exec()
+  //Table building
+  statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS building (" +
+    "id INTEGER PRIMARY KEY, " +
+    "address TEXT NOT NULL, " +
+    "complement TEXT, " +
+    "floor_nb INTEGER, " +
+    "owner_id INTEGER " +
+    ")",
+  )
+  check_err(err)
+  statement.Exec()
 
-      //Table ticket
-      statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS ticket(" +
-        "id INTEGER PRIMARY KEY, " +
-        "client_id INTEGER, " +
-        "building_id INTEGER, " +
-        "floor INTEGER, " +
-        "img_path TEXT)")
-        check_err(err)
-        statement.Exec()
-      }
+  //Table ticket
+  statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS ticket(" +
+    "id INTEGER PRIMARY KEY, " +
+    "client_id INTEGER, " +
+    "building_id INTEGER, " +
+    "floor INTEGER, " +
+    "img_path TEXT)",
+  )
+  check_err(err)
+  statement.Exec()
+}
 
-      func insert_client(first_name, last_name, phone_number, address, role string){
-        database, err := sql.Open("sqlite3", "./mydb.db")
-        check_err(err)
+func insert_client(first_name, last_name, phone_number, address, role string){
+  database, err := sql.Open("sqlite3", "./mydb.db")
+  check_err(err)
 
-        statement, err := database.Prepare("INSERT INTO client(" +
-          "first_name, last_name, phone_number, address, role)" +
-          "VALUES(?, ?, ?, ?, ?)")
-        check_err(err)
-        statement.Exec(first_name, last_name, phone_number, address, role)
-        get_client()
-      }
+  statement, err := database.Prepare("INSERT INTO client(" +
+    "first_name, last_name, phone_number, address, role)" +
+    "VALUES(?, ?, ?, ?, ?)",
+  )
+  check_err(err)
+  statement.Exec(first_name, last_name, phone_number, address, role)
+  get_client()
+}
 
-      func insert_building(address, complement string, floor_nb, owner_id int){
-        database, err := sql.Open("sqlite3", "./mydb.db")
-        check_err(err)
+func insert_building(address, complement string, floor_nb, owner_id int){
+  database, err := sql.Open("sqlite3", "./mydb.db")
+  check_err(err)
 
-        statement, err := database.Prepare("INSERT INTO building" +
-          "(address, complement, floor_nb, owner_id) VALUES" +
-          "(?, ?, ?, ?)")
-        check_err(err)
-        statement.Exec(address, complement, floor_nb, owner_id)
-      }
+  statement, err := database.Prepare("INSERT INTO building" +
+    "(address, complement, floor_nb, owner_id) VALUES" +
+    "(?, ?, ?, ?)",
+  )
+  check_err(err)
+  statement.Exec(address, complement, floor_nb, owner_id)
+}
 
-      func insert_ticket(client_id, building_id, floor int){
-        database, err := sql.Open("sqlite3", "./mydb.db")
-        check_err(err)
+func insert_ticket(client_id, building_id, floor int){
+  database, err := sql.Open("sqlite3", "./mydb.db")
+  check_err(err)
 
-        statement, err := database.Prepare("INSERT INTO ticket" +
-          "(client_id, building_id, floor) VALUES" +
-          "(?, ?, ?)")
-        check_err(err)
-        statement.Exec(client_id, building_id, floor)
-      }
+  statement, err := database.Prepare("INSERT INTO ticket" +
+    "(client_id, building_id, floor) VALUES" +
+    "(?, ?, ?)",
+  )
+  check_err(err)
+  statement.Exec(client_id, building_id, floor)
+}
 
-      func get_client() {
-        database, err := sql.Open("sqlite3", "./mydb.db")
-        check_err(err)
+func get_client() {
+  database, err := sql.Open("sqlite3", "./mydb.db")
+  check_err(err)
 
-        rows, err := database.Query("SELECT * FROM client")
-        check_err(err)
+  rows, err := database.Query("SELECT * FROM client")
+  check_err(err)
 
-        var id int
-        var first_name string
-        var last_name string
-        var phone_number string
-        var address string
-        var role string
-        for rows.Next() {
-          err = rows.Scan(&id, &first_name, &last_name, &phone_number, &address, &role)
-          fmt.Println(strconv.Itoa(id) + ": " + first_name + " " + last_name + " " + phone_number + " " + address + " " + role)
-        }
-        rows.Close()
-      }
+  var id int
+  var first_name string
+  var last_name string
+  var phone_number string
+  var address string
+  var role string
+  for rows.Next() {
+    err = rows.Scan(&id, &first_name, &last_name, &phone_number, &address, &role)
+    fmt.Println(strconv.Itoa(id) + ": " + first_name + " " + last_name + " " + phone_number + " " + address + " " + role)
+  }
+  rows.Close()
+}
 
-      func check_err(err error) {
-        if err != nil {
-          panic(err)
-        }
-      }
+func check_err(err error) {
+  if err != nil {
+    panic(err)
+  }
+}
 
-      func main() {
-        init_db()
+func main() {
+  init_db()
 
-        first_name := "Bob"
-        last_name := "Moral"
-        phone_number := "0635284956"
-        address := "2 rue des petits cailloux"
-        role := "admin"
-        insert_client(first_name, last_name, phone_number, address, role)
+  // Comment ajouter un client ==>
 
-      }
+  first_name := "Bob"
+  last_name := "Moral"
+  phone_number := "0635284956"
+  address := "2 rue des petits cailloux"
+  role := "admin"
+  insert_client(first_name, last_name, phone_number, address, role)
+}
