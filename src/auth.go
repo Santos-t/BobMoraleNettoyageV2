f12fa99c9ef289cceb4e2697cc3047ef950f303e
I. Marin <issam.marin@gmail.com>
2019-01-21 16:57:53 +0100

    [ADD] Added global variable

---
+++ src/auth.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
+
+var idUser int
+
+func contains(s []User, e string) int {
+	for i := 0; i < len(s); i++ {
+		if s[i].Email == e {
+			return i
+		}
+	}
+	return -1
+}
+func basicAuth(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+		s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+		if len(s) != 2 {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		b, err := base64.StdEncoding.DecodeString(s[1])
+		if err != nil {
+			http.Error(w, err.Error(), 401)
+			return
+		}
+
+		pair := strings.SplitN(string(b), ":", 2)
+		if len(pair) != 2 {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		clients := getClient()
+		if pair[0] != "username" || pair[1] != "password" {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+		idUser = 1
+		h.ServeHTTP(w, r)
+	}
+}
