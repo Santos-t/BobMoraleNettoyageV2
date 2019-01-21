68ab28a6acda2109d47e07e149e4324ed533cf73
Antoine LEGRAND <legrand.antoine96@gmail.com>
2019-01-21 15:11:12 +0100

    Edited function names

--- src/database.go
+++ src/database.go
@@ -1,124 +1,124 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  "strconv"
+	"database/sql"
+	"fmt"
+	"strconv"
 
-  _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Lien tuto : v
 //https://www.thepolyglotdeveloper.com/2017/04/using-sqlite-database-golang-application/
 
-func init_db() {
-  //creating and opening a local database called mydb.db using the sqlite3 driver for Go
-  database, err := sql.Open("sqlite3", "./mydb.db")
-  check_err(err)
+func initDb() {
+	//creating and opening a local database called mydb.db using the sqlite3 driver for Go
+	database, err := sql.Open("sqlite3", "./mydb.db")
+	checkErr(err)
 
-  //creating the tables of the db
-  //Table client
-  statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS client(" +
-    "id INTEGER PRIMARY KEY, " +
-    "first_name TEXT NOT NULL, " +
-    "last_name TEXT NOT NULL, " +
-    "phone_number TEXT NOT NULL, " +
-    "address TEXT, " +
-    "role TEXT " +
-    ")",
-  )
-  check_err(err)
-  statement.Exec()
+	//creating the tables of the db
+	//Table client
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS client(" +
+		"id INTEGER PRIMARY KEY, " +
+		"first_name TEXT NOT NULL, " +
+		"last_name TEXT NOT NULL, " +
+		"phone_number TEXT NOT NULL, " +
+		"address TEXT, " +
+		"role TEXT " +
+		")",
+	)
+	checkErr(err)
+	statement.Exec()
 
-  //Table building
-  statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS building (" +
-    "id INTEGER PRIMARY KEY, " +
-    "address TEXT NOT NULL, " +
-    "complement TEXT, " +
-    "floor_nb INTEGER, " +
-    "owner_id INTEGER " +
-    ")",
-  )
-  check_err(err)
-  statement.Exec()
+	//Table building
+	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS building (" +
+		"id INTEGER PRIMARY KEY, " +
+		"address TEXT NOT NULL, " +
+		"complement TEXT, " +
+		"floor_nb INTEGER, " +
+		"owner_id INTEGER " +
+		")",
+	)
+	checkErr(err)
+	statement.Exec()
 
-  //Table ticket
-  statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS ticket(" +
-    "id INTEGER PRIMARY KEY, " +
-    "client_id INTEGER, " +
-    "building_id INTEGER, " +
-    "floor INTEGER, " +
-    "img_path TEXT)",
-  )
-  check_err(err)
-  statement.Exec()
+	//Table ticket
+	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS ticket(" +
+		"id INTEGER PRIMARY KEY, " +
+		"client_id INTEGER, " +
+		"building_id INTEGER, " +
+		"floor INTEGER, " +
+		"img_path TEXT)",
+	)
+	checkErr(err)
+	statement.Exec()
 }
 
-func insert_client(first_name, last_name, phone_number, address, role string){
-  database, err := sql.Open("sqlite3", "./mydb.db")
-  check_err(err)
+func insertClient(first_name, last_name, phone_number, address, role string) {
+	database, err := sql.Open("sqlite3", "./mydb.db")
+	checkErr(err)
 
-  statement, err := database.Prepare("INSERT INTO client(" +
-    "first_name, last_name, phone_number, address, role)" +
-    "VALUES(?, ?, ?, ?, ?)",
-  )
-  check_err(err)
-  statement.Exec(first_name, last_name, phone_number, address, role)
-  get_client()
+	statement, err := database.Prepare("INSERT INTO client(" +
+		"first_name, last_name, phone_number, address, role)" +
+		"VALUES(?, ?, ?, ?, ?)",
+	)
+	checkErr(err)
+	statement.Exec(first_name, last_name, phone_number, address, role)
+	getClient()
 }
 
-func insert_building(address, complement string, floor_nb, owner_id int){
-  database, err := sql.Open("sqlite3", "./mydb.db")
-  check_err(err)
+func insertBuilding(address, complement string, floor_nb, owner_id int) {
+	database, err := sql.Open("sqlite3", "./mydb.db")
+	checkErr(err)
 
-  statement, err := database.Prepare("INSERT INTO building" +
-    "(address, complement, floor_nb, owner_id) VALUES" +
-    "(?, ?, ?, ?)",
-  )
-  check_err(err)
-  statement.Exec(address, complement, floor_nb, owner_id)
+	statement, err := database.Prepare("INSERT INTO building" +
+		"(address, complement, floor_nb, owner_id) VALUES" +
+		"(?, ?, ?, ?)",
+	)
+	checkErr(err)
+	statement.Exec(address, complement, floor_nb, owner_id)
 }
 
-func insert_ticket(client_id, building_id, floor int){
-  database, err := sql.Open("sqlite3", "./mydb.db")
-  check_err(err)
+func insertTicket(client_id, building_id, floor int) {
+	database, err := sql.Open("sqlite3", "./mydb.db")
+	checkErr(err)
 
-  statement, err := database.Prepare("INSERT INTO ticket" +
-    "(client_id, building_id, floor) VALUES" +
-    "(?, ?, ?)",
-  )
-  check_err(err)
-  statement.Exec(client_id, building_id, floor)
+	statement, err := database.Prepare("INSERT INTO ticket" +
+		"(client_id, building_id, floor) VALUES" +
+		"(?, ?, ?)",
+	)
+	checkErr(err)
+	statement.Exec(client_id, building_id, floor)
 }
 
-func get_client() {
-  database, err := sql.Open("sqlite3", "./mydb.db")
-  check_err(err)
+func getClient() {
+	database, err := sql.Open("sqlite3", "./mydb.db")
+	checkErr(err)
 
-  rows, err := database.Query("SELECT * FROM client")
-  check_err(err)
+	rows, err := database.Query("SELECT * FROM client")
+	checkErr(err)
 
-  var id int
-  var first_name string
-  var last_name string
-  var phone_number string
-  var address string
-  var role string
-  for rows.Next() {
-    err = rows.Scan(&id, &first_name, &last_name, &phone_number, &address, &role)
-    fmt.Println(strconv.Itoa(id) + ": " + first_name + " " + last_name + " " + phone_number + " " + address + " " + role)
-  }
-  rows.Close()
+	var id int
+	var first_name string
+	var last_name string
+	var phone_number string
+	var address string
+	var role string
+	for rows.Next() {
+		err = rows.Scan(&id, &first_name, &last_name, &phone_number, &address, &role)
+		fmt.Println(strconv.Itoa(id) + ": " + first_name + " " + last_name + " " + phone_number + " " + address + " " + role)
+	}
+	rows.Close()
 }
 
-func check_err(err error) {
-  if err != nil {
-    panic(err)
-  }
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
-func main() {
-  init_db()
+/*func main() {
+  initDb()
 
   // Comment ajouter un client ==>
 
@@ -128,4 +128,4 @@ func main() {
   address := "2 rue des petits cailloux"
   role := "admin"
   insert_client(first_name, last_name, phone_number, address, role)
-}
+}*/
